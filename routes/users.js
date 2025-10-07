e0f610f7b99aa0bac3a16bae33d247c2dbd6058d
NicolasdeZucca <zuccarellonj@gmail.com>
2025-10-06 23:30:43 -0300

    feat: added express-validator package, custom middleware (fields validation in req), and checks in POST method

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 const { Router } = require('express')
-const { getUsers, putUsers, postUsers, deleteUsers, patchUsers } = require('../controllers/users')
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/valid-fields')
+const { getUsers, putUsers, postUsers, deleteUsers, patchUsers } = require('../controllers/users');
 
 
 const router = Router();
 
 router.get('/', getUsers);
 
-router.post('/', postUsers);
+router.post('/', 
+    [check('nombre', 'name field is required').not().isEmpty(),
+    check('correo', 'correo not valid').isEmail(),
+    check('role', 'invalid role').isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    check('password', 'password must be contain at least 6 letters').isLength({min: 6}),
+    validarCampos
+], postUsers);
 
 router.put('/:id', putUsers);
 
